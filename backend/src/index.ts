4584b6d3780c6b6d51cd6def75f483856e90cd05
nhathao19 <141889402+nhathao19@users.noreply.github.com>
2024-07-26 14:09:57 +0700

    3- Setup Database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
-  import exprress, {Request, Response} from "express"
-  import cors from "cors"
-  import "dotenv/config"
+import exprress, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
 
-  const app = exprress();
-  app.use(exprress.json());
-  app.use(cors());
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
 
-  app.get("/test",  async (req: Request, res: Response) => {
-    res.json({message: "Hello!"})
-  })
+const app = exprress();
+app.use(exprress.json());
+app.use(cors());
 
-  app.listen(7000, ()=> {
-    console.log("server started on localhost:7000")
-  })
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello!" });
+});
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
