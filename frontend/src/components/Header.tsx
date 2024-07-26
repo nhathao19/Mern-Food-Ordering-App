34451c3274fb0c19d3d208839204f8a72720af9a
nhathao19 <141889402+nhathao19@users.noreply.github.com>
2024-07-26 16:39:09 +0700

    MainNav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import MobileNav from "./MobileNav"
+import MainNav from "./MainNav"
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
             <div className="md:hidden">
                 <MobileNav />
             </div>
+            <div className="hidden md:block"><MainNav /></div>
         </div>
     </div>
   )
