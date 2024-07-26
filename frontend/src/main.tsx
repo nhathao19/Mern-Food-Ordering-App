5d28879142df327fd5203cbb87d7de4825f5609a
nhathao19 <141889402+nhathao19@users.noreply.github.com>
2024-07-26 15:05:02 +0700

    React-route-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import AppRoutes from './AppRoutes'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AppRoutes />
+    </Router>
   </React.StrictMode>,
 )
